90d978bc59a99d3580a2901411f1d43f383b43d1
coroutinedispatcher <stavro96@gmail.com>
2024-10-07 22:05:53 +0200

    Drop  redundant code

--- challenge_3_bypass_root_detection.js
+++ challenge_3_bypass_root_detection.js
@@ -4,11 +4,6 @@ Java.perform(() => {
     const checkRoot2 = rootDetector.checkRoot2
     const checkRoot3 = rootDetector.checkRoot3
 
-    // Interestingly important for feedback
-    // if (args[0].isNull() === false) {
-    //     console.log("Non-null buffer:", args[0].readUtf8String());
-    // }    
-
     checkRoot1.implementation = function () {
         send("Bypassing checker 1")
         return false;
